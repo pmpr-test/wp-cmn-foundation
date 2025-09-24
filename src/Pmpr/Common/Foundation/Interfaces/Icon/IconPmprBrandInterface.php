0604d63f82800f17b4d2fb4a28e46766a59e6bc9
pmpr.dev <pmpr.dev@gmail.com>
2025-09-24 09:28:44 +0000

    Obfuscated

--- src/Pmpr/Common/Foundation/Interfaces/Icon/IconPmprBrandInterface.php
+++ src/Pmpr/Common/Foundation/Interfaces/Icon/IconPmprBrandInterface.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68d3b8a350b03             |
+    |             68d3b9735b181             |
     |_______________________________________|
 */
  namespace Pmpr\Common\Foundation\Interfaces\Icon; interface IconPmprBrandInterface { const yqkugkuyygummmsk = 'pmpr/1/brand/pmpr-small'; const aoceigukcccyimew = 'pmpr/1/brand/cloudflare'; }
--- src/Pmpr/Common/Foundation/ORM/Database/Schema/SmallInteger.php
+++ src/Pmpr/Common/Foundation/ORM/Database/Schema/SmallInteger.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68d3b8a350b03             |
+    |             68d3b9735b181             |
     |_______________________________________|
 */
  namespace Pmpr\Common\Foundation\ORM\Database\Schema; class SmallInteger extends Schema { public function __construct(string $aokagokqyuysuksm, ?string $meqocwsecsywiiqs = null) { $this->omqeokeyimawioym('smallint'); parent::__construct('smallInteger', $aokagokqyuysuksm, $meqocwsecsywiiqs); } }
